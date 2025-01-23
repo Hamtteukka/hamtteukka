d2df5ee67adbf37f4d54c3a9bb9da8366dcdf1bc
김준하 <junhakjh@ajou.ac.kr>
2025-01-23 15:16:23 +0900

    feat(S12P11A608-120): DragAndDropFile 컴포넌트 개발

---
+++ frontend/src/components/ui/dranAndDropUpload/DragAndDropUpload.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import dynamic from 'next/dynamic';
+
+const DragAndDropUploadImg = dynamic(() => import('@/components/ui/dranAndDropUpload/DragAndDropUpload'));
+
+const meta = {
+  title: 'ui/DragAndDropUpload',
+  component: DragAndDropUploadImg,
+  argTypes: {
+    setImage: {
+      description: '이미지 파일 핸들러 함수',
+    },
+    className: { control: 'text' },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof DragAndDropUploadImg>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DragAndDropUploadImgStory: Story = {
+  args: {
+    setImage: () => {},
+  },
+};
