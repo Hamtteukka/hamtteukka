52c873897557496cd24bb8131aff6b4199da1e58
우성문 <tjdans7665@naver.com>
2025-01-26 13:03:08 +0900

    feat(S12P11A608-99): JWT(토큰 생성, 검증), 필터 기반 검증 로직 구현

--- backend/src/main/java/com/ssafy/hamtteukka/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/hamtteukka/config/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.ssafy.hamtteukka.config;
 
+import com.ssafy.hamtteukka.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (테스트 환경에서는 사용하지 않아도 됨)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/swagger-ui/**", "/v3/api-docs","/v3/api-docs/**", "/swagger-ui.html" // Swagger 경로 허용
-
+                                "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", // Swagger 경로 허용
+                                "/auth/**","/users/**","/feeds/**","/comments","/ai/**", "/test/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form.disable()); // 폼 로그인 비활성화
         return http.build();
     }
---
+++ backend/src/main/java/com/ssafy/hamtteukka/security/JwtTokenProvider.java
@@ -0,0 +1,79 @@
+    package com.ssafy.hamtteukka.security;
+
+    import io.jsonwebtoken.*;
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.stereotype.Component;
+
+    import java.util.Date;
+
+    @Component
+    public class JwtTokenProvider {
+        @Value("${JWT_SECRETKEY}")
+        private String secretKey;
+
+        /**
+         * JWT 발급 메서드
+         * @param id 페이로드에 저장할 id 값
+         * @param expiration 유효 시간(분)
+         * @return 새로운 JWT 토큰
+         */
+        public String generateJwt(long id, int expiration) {
+            return Jwts
+                    .builder()
+                    .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                    .claim("id", id)
+                    // System.currentTimeMillis(): 1/1000초(ms) 단위의 현재 시간을 반환
+                    // 현재 시간부터 expiration분 후 만료
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * expiration))
+                    .signWith(SignatureAlgorithm.HS256, secretKey)
+                    .compact();
+        }
+
+
+        /**
+         * JWT Token 검증 메서드
+         * @param token JWT 토큰
+         * @return JWT 유효 여부
+         */
+        public boolean validToken(String token) {
+            try {
+                Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(resolveToken(token));
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        /**
+         * JWT Token Claims(페이로드 부) 정보 파싱 메서드
+         * @param token
+         * @return JWT 토큰의 페이로드
+         */
+        private Claims getClaims(String token) {
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(resolveToken(token))
+                    .getBody();
+        }
+
+        /**
+         * JWT Token Claims(페이로드 부) 정보에서 id 값 추출 메서드
+         * @param token
+         * @return 페이로드에서 추출한 id 값
+         */
+        public long getIdFromToken(String token) {
+            Claims claims = getClaims(token);
+            return claims.get("id", Long.class);
+        }
+
+        private String resolveToken(String token) {
+            if (token != null && token.startsWith("Bearer ")) {
+                return token.substring(7);
+            }
+            return null;
+        }
+    }
