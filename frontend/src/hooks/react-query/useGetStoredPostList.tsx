8c5be5a0f006dc98ad18721cafbc04426b446e1a
김준하 <junhakjh@ajou.ac.kr>
2025-01-30 16:57:29 +0900

    feat(S12P11A608-169): 저장한 게시물 탭 개발

--- frontend/src/hooks/react-query/useGetStoredPostList.tsx
+++ frontend/src/hooks/react-query/useGetStoredPostList.tsx
@@ -5,12 +5,12 @@ import { TCursorData } from '@/types/service';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 export const useGetStoredPostList = () => {
-  const { data, isPending, isError, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const { data, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: [queryKey.STORED_POST],
     queryFn: ({ pageParam }) => getStoredPostList(pageParam, POST_LIMIT),
     initialPageParam: -1,
     getNextPageParam: (lastPage: TCursorData<TPostPreview>) => (lastPage?.hasNextItems ? lastPage?.nextCursorId : null),
   });
 
-  return { data, isPending, isError, fetchNextPage, hasNextPage };
+  return { data, isFetching, fetchNextPage, hasNextPage };
 };
