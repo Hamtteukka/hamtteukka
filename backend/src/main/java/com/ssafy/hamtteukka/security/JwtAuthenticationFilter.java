e54f0aee2c8fb0d9359587703a3217d3f45efef5
조강민 <jkm96529@naver.com>
2025-02-09 18:59:55 +0900

    chore : jwtfileter에 테스트로 /ai/를 token 없어도 되는 path

--- backend/src/main/java/com/ssafy/hamtteukka/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/hamtteukka/security/JwtAuthenticationFilter.java
@@ -39,7 +39,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             "/users",
             "/openvidu",
             "/auth",
-            "/test/"
+            "/test/",
+            "/ai/" // test로
     );
 
     protected boolean checkPath(HttpServletRequest request) {
