36f195b8c7f1a11721eaf6fb9b7b79f56e5ad9f8
서로 <vholls2@naver.com>
2025-02-11 10:29:04 +0900

    fix: 홈 피드 API 요청 엔드포인트 변경

--- frontend/src/components/page/archive/SubscriptionList.tsx
+++ frontend/src/components/page/archive/SubscriptionList.tsx
@@ -1,8 +1,16 @@
 import SubscriptionProfile from '@/components/ui/profile/SubscriptionProfile';
 import { MSubscriptionList } from '@/mocks/data/archive';
+import { TSubscriptionProfile } from '@/types/archive';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const SubscriptionList: React.FC = () => {
+  const [subscriptionList, setSubscriptionList] = useState<TSubscriptionProfile[]>([]);
+
+  useEffect(() => {
+    const fetchSubscriptionList = async () => {};
+  }, []);
+
   return (
     <section className='grid grid-cols-1 gap-10 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {MSubscriptionList.map((item) => (
