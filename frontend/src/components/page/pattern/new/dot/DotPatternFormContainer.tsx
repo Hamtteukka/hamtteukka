3306344220d62c90c8ecdb6b7a4da79539fb8103
우성문 <tjdans7665@naver.com>
2025-02-20 17:39:03 +0900

    design: updated images across various sections

--- frontend/src/components/page/pattern/new/dot/DotPatternFormContainer.tsx
+++ frontend/src/components/page/pattern/new/dot/DotPatternFormContainer.tsx
@@ -4,6 +4,7 @@ const DotPatternFormContainer: React.FC = () => {
   return (
     <section className='flex grow flex-col gap-4'>
       <p className='text-detail text-deepgray'>도트 도안 - 참고 이미지와 비슷한 도트 도안이 생성돼요.</p>
+      <p className='text-detail text-deepgray -mt-[15px]'>대바늘은 다양한 색상을 사용할 경우 난이도가 높아져요!</p>
       <DotPatternForm />
     </section>
   );
